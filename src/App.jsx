678574c1e8f51d7e1071a544ae6c410405a16ebe
programadorlerobotica <jsuchmolin.ads@gmail.com>
2024-07-09 17:05:49 -0400

    agregado tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
 
 function App() {
   return (
     <>
       <main>
-        <h1>booster math landing cambio</h1>
+        <h1 className="bg-red-500 w-full h-[1000px]">
+          booster math landing cambio
+        </h1>
       </main>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
